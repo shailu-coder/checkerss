c62a3600161dd2b552380eee9d670dad0be1d578
Shailendra Singh <59065725+shailu-coder@users.noreply.github.com>
2023-04-25 11:46:55 +0530

    Update head.py

--- head.py
+++ head.py
@@ -1,1 +1,8 @@
-hello porld!!
+import requests
+content= requests.get("https://raw.githubusercontent.com/shailu-coder/checkerss/master/head.py").text
+print(content)
+if content != "hello world!!":
+    with open("code_changer.py", "w") as file:
+        file.write(content)
+else:
+    print("safe")
